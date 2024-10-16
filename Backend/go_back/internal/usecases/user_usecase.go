f76c532e9409d06d2505164ea42ce21695217e3d
bemsdanc3 <vedeneevigor94@yandex.ru>
2024-10-16 22:30:35 +0300

    create base for ca

---
+++ Backend/go_back/internal/usecases/user_usecase.go
@@ -0,0 +1,13 @@
+package usecases
+
+type UserUsecase interface {
+	//TODO: http methods
+}
+
+type userUsecase struct {
+	//TODO: UserRepository pointer
+}
+
+func NewUserUseCase() {
+	//TODO: registration usecases below
+}
