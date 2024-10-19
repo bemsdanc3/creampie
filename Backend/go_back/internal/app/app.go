98255acb0ef58f7107bae76a8da31d2f09fc9e5a
bemsdanc3 <vedeneevigor94@yandex.ru>
2024-10-19 11:52:29 +0300

    post method

--- Backend/go_back/internal/app/app.go
+++ Backend/go_back/internal/app/app.go
@@ -2,6 +2,9 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
+	"go_back/internal/delivery/http"
+	"go_back/internal/repository"
+	"go_back/internal/usecases"
 	"go_back/pkg/db"
 	"log"
 )
@@ -11,8 +14,15 @@ func Run() {
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-
+	//инициализация репозиториев
+	userRepo := repository.NewUserRepository(database)
+	//инициализация юзкейсов
+	userUsecase := usecases.UserUsecase(userRepo)
 	r := gin.Default()
+
+	//обработка пользовательских запросов
+	userHandler := http.NewUserHandler(userUsecase)
+	r.POST("/users", userHandler.CreateUser)
 
 	if err := r.Run(":3002"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
